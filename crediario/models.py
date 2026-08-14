36b72a418e231075172e6bcc10865efdf2708cb9
Matheus Henrique <teus.henrique021@gmail.com>
2026-08-14 11:28:11 -0300

    feat: create models from crediario

--- crediario/models.py
+++ crediario/models.py
@@ -1,3 +1,81 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(
+        max_length=150
+    )
+
+    cpf = models.CharField(
+        max_length=14,
+        unique=True
+    )
+
+    telefone = models.CharField(
+        max_length=20,
+        blank=True
+    )
+
+    limiteCrediario = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+
+    ativo = models.BooleanField(
+        default=True
+    )
+
+    def __str__(self):
+        return f'{self.nome}'
+
+class Credito(models.Model):
+    cliente = models.ForeignKey(
+        Cliente,
+        on_delete=models.PROTECT,
+        related_name='creditos'
+    )
+
+    valorTotal = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    data = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    observacao = models.CharField(
+        max_length=50,
+        blank=True
+    )
+
+    def __str__(self):
+        return f'{self.cliente} - R% {self.valorTotal}'
+
+class Parcela(models.Model):
+    credito = models.ForeignKey(
+        Credito,
+        on_delete=models.PROTECT,
+        related_name='parcelas'
+    )
+
+    numero = models.PositiveBigIntegerField()
+
+    valor = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    dataVencimento = models.DateField()
+
+    dataPagamento = models.DateField(
+        blank=True,
+        null=True
+    )
+
+    paga = models.BooleanField(
+        default=False
+    )
+
+    def __str__(self):
+        return f'Parcela {self.numero} - {self.credito.cliente.nome}'
