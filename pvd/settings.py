41f375072c1af5a7e859745f409c3ca31da130ec
Matheus Henrique <teus.henrique021@gmail.com>
2026-08-16 08:48:41 -0300

    feat: configure Django Templates and add base layout

--- pvd/settings.py
+++ pvd/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'pvd.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
